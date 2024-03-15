7fb36c660adfd5bb72e41d7db91aa41fd12c843f
JaredMel <jareddmelendez1764@gmail.com>
2024-03-15 14:32:33 -0700

    WorkinProgress

--- main.hpp
+++ main.hpp
@@ -1,3 +1,60 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int writeFile(string file)
+{
+    ofstream ofs;
+    int N;
+    int ID;
+    string Ename;
+    string Dname;
+    double S;
+
+    cin >> N;
+    ofs.open(file);
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+    else
+    {
+        ofs << N << endl;
+    }
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> ID >> Ename >> Dname >> S;
+        if (ofs)
+        {
+            ofs << ID << " " << Ename << " " << Dname << " " << S << endl;
+        }
+    }
+    ofs.close();
+    return N;
+}
+int readFile(string file)
+{
+    ifstream ifs;
+    int N;
+    int ID;
+    string Ename;
+    string Dname;
+    double S;
+
+    ifs.open(file);
+    if (!ifs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+    else
+    {
+        ifs >> N;
+    }
+    for (size_t i = 0; i < N; i++)
+    {
+        ifs >> ID >> Ename >> Dname >> S;
+        cout << ID << " " << Ename << " " << Dname << " " << S << endl;
+    }
+    return N;
+}
